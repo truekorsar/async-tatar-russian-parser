993593e37614853932b036f0d6a0ad7df3b47865
rtconverter <rtconverter@gmail.com>
2020-09-25 15:57:52 +0300

    Added annotations

--- parser.py
+++ parser.py
@@ -1,81 +1,92 @@
 """
-Парсинг слов с сайта 'http://tatar_russian.academic.ru/' и запись в csv и json файлы
+Scraping words from 'http://tatar_russian.academic.ru/' and writing them in csv and json files
 """
 import asyncio
 import aiohttp
 import time
 import json
 import csv
+import bs4
 from copy import copy
-from bs4 import BeautifulSoup as BS
+from typing import Dict
 
-words = {}  # Словарь, куда помещаются слова с переводами
+# Type annotations
+LinkToSectionAsTag = bs4.element.Tag
+LinksToSections = bs4.element.ResultSet
+LinkToPaginationPageAsTag = bs4.element.Tag
+LinksToPaginationPages = bs4.element.ResultSet
+LinkToWordPageAsTag = bs4.element.Tag
+LinksToWordPages = bs4.element.ResultSet
+
+words: Dict[str, str] = {}  # Dict to place words in. Key is actual tatar word, value - translation
 JSON_FILENAME = "tatar-russian.json"
 CSV_FILENAME = "tatar-russian.csv"
-MAX_CONCURRENT = 4500  # Максимальное число задач по парсингу слов, работающих одновременно (get_word_info)
-MAIN_PAGE = 'https://tatar_russian.academic.ru/'  # Главна страница словаря
-PARSER = 'lxml'  # Парсер
+MAX_CONCURRENT = 4500  # Maximum number of tasks 'get_word_info' working concurrently
+MAIN_PAGE = 'https://tatar_russian.academic.ru/'
+PARSER = 'lxml'
 
 
-def write_csv(filename):
-    """Запись в csv файл"""
+def write_csv(filename: str) -> None:
+    """Write in csv file"""
     with open(filename, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(words.items())
 
 
-def write_json(filename):
-    """Запись в json файл"""
+def write_json(filename: str) -> None:
+    """Write in json file"""
     with open(filename, "w") as file:
         json.dump(words, file)
 
 
-def write_both(json_filename, csv_filename):
-    """Запись в оба файла"""
+def write_both(json_filename: str, csv_filename: str) -> None:
+    """Write in both"""
     write_json(json_filename)
     write_csv(csv_filename)
 
 
-async def get_section_links(session):
-    """Получение ссылок на разделы"""
+async def get_section_links(session: aiohttp.ClientSession) -> LinksToSections:
+    """Fetching links to sections"""
     async with session.request('get', MAIN_PAGE, verify_ssl=False) as resp:
         main_html = await resp.text()
-    sections = BS(main_html, PARSER).find('div', class_='contents-wrap').find_all('a')
+    sections = bs4.BeautifulSoup(main_html, PARSER).find('div', class_='contents-wrap').find_all('a')
     return sections
 
 
-async def get_pagination_pages_links(session, section):
-    """Получение ссылок на страницы пагинации со страницы раздела"""
+async def get_pagination_pages_links(session: aiohttp.ClientSession,
+                                     section: LinkToSectionAsTag) -> LinksToPaginationPages:
+    """Fetching links to pagination pages from the section page"""
     async with session.request('get', MAIN_PAGE + section.attrs['href'], verify_ssl=False) as resp:
         section_html = await resp.text()
-    pagination_pages = BS(section_html, PARSER).\
-        find('div', class_='page-nav'). \
-        find_all('ul')[-1].find_all('a')
-    #  Формирование первой страницы пагинации
+    pagination_pages = (bs4.BeautifulSoup(section_html, PARSER).
+                        find('div', class_='page-nav').
+                        find_all('ul')[-1].find_all('a'))
+    # Building link to first pagination page
     last_page = pagination_pages[-1]
     first_page = copy(last_page)
     last_page_href = last_page.attrs['href']
-    first_page.attrs['href'] = last_page_href[:last_page_href.rfind('&')]  # Ссылка на первую страницу пагинации
+    first_page.attrs['href'] = last_page_href[:last_page_href.rfind('&')]  # Actual link to first pagination page
     pagination_pages.append(first_page)
     return pagination_pages
 
 
-async def get_word_links(session, pagination_page):
-    """Получение ссылок на слова со страницы пагинации """
+async def get_word_links(session: aiohttp.ClientSession,
+                         pagination_page: LinkToSectionAsTag) -> LinksToWordPages:
+    """Fetching links to words from the pagination page"""
     async with session.request('get', MAIN_PAGE + pagination_page.attrs['href'], verify_ssl=False) as resp:
         pagination_page_html = await resp.text()
-    word_links = BS(pagination_page_html, PARSER).find('div', 'terms-wrap').find_all('a')
+    word_links = bs4.BeautifulSoup(pagination_page_html, PARSER).find('div', 'terms-wrap').find_all('a')
     return word_links
 
 
-async def get_word_info(session, word):
-    """Получение слова и его перевода используя ссылку на это слово"""
+async def get_word_info(session: aiohttp.ClientSession, word: LinkToWordPageAsTag) -> None:
+    """Fetching tatar word and it's translation from the link to word and write this info to dict 'words'"""
     async with session.request('get', word.attrs['href'], verify_ssl=False) as resp:
         word_html = await resp.text()
-    tatar_word = BS(word_html, PARSER).find('div', id='TerminTitle').find('h1').text
+    tatar_word = bs4.BeautifulSoup(word_html, PARSER).find('div', id='TerminTitle').find('h1').text
 
-    #  Формирование перевода слова
-    lines = BS(word_html, PARSER).find('dd', class_='descript').find_all('div')
+    #  Building valid word translation
+    lines = bs4.BeautifulSoup(word_html, PARSER).find('dd', class_='descript').find_all('div')
     translation = ""
     for line in lines:
         if line.text.endswith('.'):
@@ -84,13 +95,12 @@ async def get_word_info(session, word):
             translation += line.text + '\n'
     if len(translation) > 0:
         print(tatar_word, translation)
-        words[tatar_word] = translation  # Запись в словарь
+        words[tatar_word] = translation
 
 
 async def main():
     try:
         async with aiohttp.ClientSession() as session:
-
             print("Collecting links to sections...")
             sections = await get_section_links(session)
 
@@ -106,7 +116,7 @@ async def main():
 
             print("Collecting words (30 - 40 min)...\n\n")
 
-            #  Запускаем задачи по получению слов по частям, иначе сервер может перестать отвечать
+            #  Launching tasks 'get_word_info' in parts, as otherwise server may close the connection
             tasks = []
             chunk_start_position = 0
             for pagination_page in all_word_links:
