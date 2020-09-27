d091e6e54efdf8b1aeaded656e4b0ec5a63b9231
rtconverter <rtconverter@gmail.com>
2020-09-27 17:07:47 +0300

    Refactored, added semaphore

--- parser.py
+++ parser.py
@@ -10,18 +10,10 @@ import bs4
 from copy import copy
 from typing import Dict
 
-# Type annotations
-LinkToSectionAsTag = bs4.element.Tag
-LinksToSections = bs4.element.ResultSet
-LinkToPaginationPageAsTag = bs4.element.Tag
-LinksToPaginationPages = bs4.element.ResultSet
-LinkToWordPageAsTag = bs4.element.Tag
-LinksToWordPages = bs4.element.ResultSet
-
 words: Dict[str, str] = {}  # Dict to place words in. Key is actual tatar word, value - translation
 JSON_FILENAME = "tatar-russian.json"
 CSV_FILENAME = "tatar-russian.csv"
-MAX_CONCURRENT = 4500  # Maximum number of tasks 'get_word_info' working concurrently
+MAX_CONCURRENT = 3000  # Maximum number of tasks working concurrently
 MAIN_PAGE = 'https://tatar_russian.academic.ru/'
 PARSER = 'lxml'
 
@@ -45,7 +37,7 @@ def write_both(json_filename: str, csv_filename: str) -> None:
     write_csv(csv_filename)
 
 
-async def get_section_links(session: aiohttp.ClientSession) -> LinksToSections:
+async def get_section_links(session: aiohttp.ClientSession) -> bs4.element.ResultSet:
     """Fetching links to sections"""
     async with session.request('get', MAIN_PAGE, verify_ssl=False) as resp:
         main_html = await resp.text()
@@ -53,10 +45,11 @@ async def get_section_links(session: aiohttp.ClientSession) -> LinksToSections:
     return sections
 
 
-async def get_pagination_pages_links(session: aiohttp.ClientSession,
-                                     section: LinkToSectionAsTag) -> LinksToPaginationPages:
-    """Fetching links to pagination pages from the section page"""
-    async with session.request('get', MAIN_PAGE + section.attrs['href'], verify_ssl=False) as resp:
+async def process_section(semaphore: asyncio.Semaphore,
+                          session: aiohttp.ClientSession,
+                          section: bs4.element.Tag) -> None:
+    """Fetching links to pagination pages from the section page and launch 'process_pagination_page' tasks"""
+    async with semaphore, session.request('get', MAIN_PAGE + section.attrs['href'], verify_ssl=False) as resp:
         section_html = await resp.text()
     pagination_pages = (bs4.BeautifulSoup(section_html, PARSER).
                         find('div', class_='page-nav').
@@ -67,21 +60,26 @@ async def get_pagination_pages_links(session: aiohttp.ClientSession,
     last_page_href = last_page.attrs['href']
     first_page.attrs['href'] = last_page_href[:last_page_href.rfind('&')]  # Actual link to first pagination page
     pagination_pages.append(first_page)
-    return pagination_pages
+    tasks = [process_pagination_page(semaphore, session, pagination_page) for pagination_page in pagination_pages]
+    await asyncio.gather(*tasks)
 
 
-async def get_word_links(session: aiohttp.ClientSession,
-                         pagination_page: LinkToSectionAsTag) -> LinksToWordPages:
-    """Fetching links to words from the pagination page"""
-    async with session.request('get', MAIN_PAGE + pagination_page.attrs['href'], verify_ssl=False) as resp:
+async def process_pagination_page(semaphore: asyncio.Semaphore,
+                                  session: aiohttp.ClientSession,
+                                  pagination_page: bs4.element.Tag) -> None:
+    """Fetch links to words from the pagination page and launch 'process_word_link' tasks"""
+    async with semaphore, session.request('get', MAIN_PAGE + pagination_page.attrs['href'], verify_ssl=False) as resp:
         pagination_page_html = await resp.text()
     word_links = bs4.BeautifulSoup(pagination_page_html, PARSER).find('div', 'terms-wrap').find_all('a')
-    return word_links
+    tasks = [process_word_link(semaphore, session, word_link) for word_link in word_links]
+    await asyncio.gather(*tasks)
 
 
-async def get_word_info(session: aiohttp.ClientSession, word: LinkToWordPageAsTag) -> None:
-    """Fetching tatar word and it's translation from the link to word and write this info to dict 'words'"""
-    async with session.request('get', word.attrs['href'], verify_ssl=False) as resp:
+async def process_word_link(semaphore: asyncio.Semaphore,
+                            session: aiohttp.ClientSession,
+                            word: bs4.element.Tag) -> None:
+    """Fetch tatar word and it's translation from the link to word and write this info to dict 'words'"""
+    async with semaphore, session.request('get', word.attrs['href'], verify_ssl=False) as resp:
         word_html = await resp.text()
     tatar_word = bs4.BeautifulSoup(word_html, PARSER).find('div', id='TerminTitle').find('h1').text
 
@@ -100,30 +98,12 @@ async def get_word_info(session: aiohttp.ClientSession, word: LinkToWordPageAsTa
 
 async def main():
     try:
+        # Semaphore control needed, as otherwise server may close the connection
+        semaphore = asyncio.Semaphore(value=MAX_CONCURRENT)
         async with aiohttp.ClientSession() as session:
-            print("Collecting links to sections...")
             sections = await get_section_links(session)
-
-            print("Collecting links to pagination pages...")
-            tasks = [get_pagination_pages_links(session, section) for section in sections]
-            all_pagination_pages = await asyncio.gather(*tasks)
-
-            print("Collecting links to words (2-3 min)...")
-            tasks = []
-            for section in all_pagination_pages:
-                tasks.extend([get_word_links(session, pagination_page) for pagination_page in section])
-            all_word_links = await asyncio.gather(*tasks)
-
-            print("Collecting words (30 - 40 min)...\n\n")
-
-            #  Launching tasks 'get_word_info' in parts, as otherwise server may close the connection
-            tasks = []
-            chunk_start_position = 0
-            for pagination_page in all_word_links:
-                tasks.extend([get_word_info(session, word) for word in pagination_page])
-            while chunk_start_position < len(tasks):
-                await asyncio.gather(*tasks[chunk_start_position:chunk_start_position+MAX_CONCURRENT])
-                chunk_start_position += MAX_CONCURRENT
+            tasks = [process_section(semaphore, session, section) for section in sections]
+            await asyncio.gather(*tasks)
 
     except (asyncio.TimeoutError, aiohttp.ServerDisconnectedError):
         print('Error occurred, all progress will be saved. Try to reduce MAX_CONCURRENT')
